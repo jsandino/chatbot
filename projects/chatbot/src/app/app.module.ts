d0f7aae3b86da8c36b123613340aa73ae051e83f
Javier Sandino <javier@sandino.ca>
2025-05-24 11:24:16 -0400

    Minor simplifications & clean-up.

--- projects/chatbot/src/app/app.module.ts
+++ projects/chatbot/src/app/app.module.ts
@@ -1,21 +1,21 @@
 import { NgModule, Injector, DoBootstrap } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-// import { createCustomElement } from '@angular/elements';
+import { createCustomElement } from '@angular/elements';
 import { ChatbotLoaderComponent } from './chatbot-loader/chatbot-loader.component';
-
 
 @NgModule({
   imports: [BrowserModule],
   declarations: [ChatbotLoaderComponent],
   providers: [],
 })
-export class AppModule {}
-// export class AppModule implements DoBootstrap {
-//   constructor(private readonly injector: Injector) {}
+export class AppModule implements DoBootstrap {
+  constructor(private readonly injector: Injector) {}
 
-//   ngDoBootstrap() {
-//     console.log('Bootstrapping chatbot-loader...');
-//     const el = createCustomElement(ChatbotLoaderComponent, { injector: this.injector });
-//     customElements.define('chatbot-loader', el);
-//   }
-// }
+  ngDoBootstrap() {
+    console.log('Bootstrapping chatbot-loader...');
+    const el = createCustomElement(ChatbotLoaderComponent, {
+      injector: this.injector,
+    });
+    customElements.define('chatbot-loader', el);
+  }
+}
--- projects/chatbot/src/main.ts
+++ projects/chatbot/src/main.ts
@@ -1,4 +1,6 @@
-import './app/app.module';
-import { defineCustomElements } from './custom-elements';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-defineCustomElements();
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch(err => console.error(err));
